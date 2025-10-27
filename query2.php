b884c298271153b66d9e47f2770635c540ed0ac3
Nymul Seam <seamnymul@gmail.com>
2025-10-27 11:42:49 +1100

    Update query2.php

--- query2.php
+++ query2.php
@@ -1,35 +1,50 @@
 <html>
-     <body>
-          <?php
-               include './parameters/get-parameters.php';
-               $_pick = $_POST['selection'];
+    <body>
+        <?php
+            // Include secure database connection
+            include 'db.php';
 
-               switch ($_pick) {
-                    case "Q1":
+            // Sanitize and validate user input
+            include './parameters/get-parameters.php';
+            $_pick = htmlspecialchars(trim($_POST['selection']));
 
-                         include 'mobile.php';
-                         break;
+            // Handle empty selection
+            if (empty($_pick)) {
+                echo "<p>Please select a valid query option.</p>";
+                exit;
+            }
 
-                    case "Q2":
-                         include 'population.php';
-                         break;
+            // Route to the correct data file
+            switch ($_pick) {
+                case "Q1":
+                    include 'mobile.php';
+                    break;
 
-                    case "Q3":
-                         include 'lifeexpectancy.php';
-                         break;
+                case "Q2":
+                    include 'population.php';
+                    break;
 
-                    case "Q4":
-                         include 'gdp.php';
-                         break;
+                case "Q3":
+                    include 'lifeexpectancy.php';
+                    break;
 
-                    case "Q5":
-                         include 'mortality.php';
-                         break;
-               }
-          ?>
+                case "Q4":
+                    include 'gdp.php';
+                    break;
 
-          <div id="Copyright" class="center">
-               <h5>&copy; 2023, Bluetech Group. All rights reserved.</h5>
-          </div>
-     </body>
+                case "Q5":
+                    include 'childmortality.php'; // Updated filename
+                    break;
+
+                default:
+                    echo "<p>Invalid selection. Please try again.</p>";
+            }
+        ?>
+
+        <div id="Copyright" class="center">
+            <h5>&copy; 2023, Bluetech Group. All rights reserved.</h5>
+        </div>
+    </body>
 </html>
+
+
