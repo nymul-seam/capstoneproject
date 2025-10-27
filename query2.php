7a78407eed3d6df62b2f853d930dccf40d83e5e5
Nymul Seam <seamnymul@gmail.com>
2025-10-27 12:35:55 +1100

    Refactor query2.php for improved structure

--- query2.php
+++ query2.php
@@ -1,52 +1,39 @@
-<!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <title>Query Results</title>
-</head>
-<body>
-    <?php
-        // Include secure database connection
-        include 'db.php';
+     <body>
+          <?php
+               include './parameters/get-parameters.php';
+               $_pick = $_POST['selection'];
 
-        // Sanitize and validate user input
-        $_pick = isset($_POST['selection']) ? htmlspecialchars(trim($_POST['selection'])) : '';
+               switch ($_pick) {
+                    case "Q1":
 
-        if (empty($_pick)) {
-            echo "<p>Please select a valid query option.</p>";
-            exit;
-        }
+                         include 'mobile.php';
+                         break;
 
-        // Route to the correct data file
-        switch ($_pick) {
-            case "Q1":
-                include 'mobile.php';
-                break;
-            case "Q2":
-                include 'population.php';
-                break;
-            case "Q3":
-                include 'lifeexpectancy.php';
-                break;
-            case "Q4":
-                include 'gdp.php';
-                break;
-            case "Q5":
-                include 'childmortality.php'; // Ensure this filename matches your repo
-                break;
-            default:
-                echo "<p>Invalid selection. Please try again.</p>";
-        }
-    ?>
+                    case "Q2":
+                         include 'population.php';
+                         break;
 
-    <br><a href="query.php">Back to Query Page</a>
-    <br><a href="index.php">Home</a>
+                    case "Q3":
+                         include 'lifeexpectancy.php';
+                         break;
 
-    <footer>
-        <p>&copy; 2023, Bluetech Group. All rights reserved.</p>
-    </footer>
-</body>
+                    case "Q4":
+                         include 'gdp.php';
+                         break;
+
+                    case "Q5":
+                         include 'mortality.php';
+                         break;
+               }
+          ?>
+
+          <div id="Copyright" class="center">
+               <h5>&copy; 2023, Bluetech Group. All rights reserved.</h5>
+          </div>
+     </body>
 </html>
+
 
 
 
