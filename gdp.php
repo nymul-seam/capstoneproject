62b89f128c3d6b9fe1835585e0c1d976ab763ad7
Nymul Seam <seamnymul@gmail.com>
2025-10-28 14:36:20 +1100

    Integrate updated GDP per Capita data table
    Added a new GDP per Capita table alongside legacy data.

--- gdp.php
+++ gdp.php
@@ -9,45 +9,44 @@
   <input type="submit" id="pickquery" value="Pick another query">
 </form>
 
- <?php
-       include 'get-parameters.php';
+<?php
+include 'get-parameters.php';
 
-       //Query to get the GDP
-        $sql = "select name, gdp from countrydata_table;";
-      //  $conn = new mysqli($_SESSION['ep'], $_SESSION['un'], $_SESSION['pw'], $_SESSION['db']);
-        $conn = mysqli_init();
-        mysqli_ssl_set($conn,NULL,NULL, "$sslcert", NULL, NULL);
-        mysqli_real_connect($conn, "$host", "$username", "$password", "$db_name", 3306, MYSQLI_CLIENT_SSL);
+// Original query
+$sql = "select name, gdp from countrydata_table;";
+$conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL, "$sslcert", NULL, NULL);
+mysqli_real_connect($conn, "$host", "$username", "$password", "$db_name", 3306, MYSQLI_CLIENT_SSL);
 
-       if ($conn->connect_error) {
-            error_log('Connection error: ' . $conn->connect_error);
-            var_dump('Connection error: ' . $conn->connect_error);
+if ($conn->connect_error) {
+    error_log('Connection error: ' . $conn->connect_error);
+    var_dump('Connection error: ' . $conn->connect_error);
+} else {
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        echo '<h3>Legacy GDP Data</h3>';
+        echo '<table style="width: 80%">';
+        echo '<tr><th style="text-align:left">This is a Country Name</th><th style="text-align:left">Gross Domestic Product</th></tr>';
+        while($row = $result->fetch_assoc()) {
+            echo '<tr><td>' . $row["name"] . '&nbsp</td><td>' . $row["gdp"] . '&nbsp</td></tr>';
         }
-        else {
-          $result = $conn->query($sql);
-          if ($result->num_rows > 0) {
+        echo '</table>';
+    }
 
-              echo '<table style="width: 80%">';
-              echo '<tr>';
-              echo '<th style="text-align:left">This is a Country Name</th>';
-              echo '<th style="text-align:left">Gross Domestic Product</th>';
-              echo '</tr>';
-
-              while($row = $result->fetch_assoc()) {
-
-              echo '<tr>';
-              echo '<td>';
-              echo $row["name"];
-              echo '&nbsp';
-              echo '<td>';
-              echo $row["gdp"];
-              echo '&nbsp';
-              echo '<br>';
-              echo '</tr>';
-              }
-              echo '</table>';
-            }
+    // New GDP table integration
+    $sql2 = "SELECT country, year, gdp_per_capita FROM gdp ORDER BY year ASC";
+    $result2 = $conn->query($sql2);
+    if ($result2->num_rows > 0) {
+        echo '<h3>Updated GDP per Capita</h3>';
+        echo '<table style="width: 80%">';
+        echo '<tr><th style="text-align:left">Country</th><th style="text-align:left">Year</th><th style="text-align:left">GDP per Capita</th></tr>';
+        while($row = $result2->fetch_assoc()) {
+            echo '<tr><td>' . $row["country"] . '</td><td>' . $row["year"] . '</td><td>' . $row["gdp_per_capita"] . '</td></tr>';
         }
+        echo '</table>';
+    }
+}
+?>
 
 
-    ?>
+
