083e31c56912f0c7ba72a5160908459cb44bc085
Nymul Seam <seamnymul@gmail.com>
2025-10-28 14:37:24 +1100

    Enhance mobile.php to include subscriptions data
    Added functionality to display updated mobile subscriptions data.

--- mobile.php
+++ mobile.php
@@ -1,4 +1,3 @@
-
 <style>
 #pickquery {
      height: 50px;
@@ -10,45 +9,42 @@
   <input type="submit" id="pickquery" value="Pick another query">
 </form>
 
- <?php
-         include 'get-parameters.php';
+<?php
+include 'get-parameters.php';
 
-        //Query to get the GDP
-        $sql = "select name, mobilephones from countrydata_table;";
-//        $conn = new mysqli($_SESSION['ep'], $_SESSION['un'], $_SESSION['pw'], $_SESSION['db']);
-	 $conn = mysqli_init();
-        mysqli_ssl_set($conn,NULL,NULL, "$sslcert", NULL, NULL);
-        mysqli_real_connect($conn, "$host", "$username", "$password", "$db_name", 3306, MYSQLI_CLIENT_SSL);
+$sql = "select name, mobilephones from countrydata_table;";
+$conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL, "$sslcert", NULL, NULL);
+mysqli_real_connect($conn, "$host", "$username", "$password", "$db_name", 3306, MYSQLI_CLIENT_SSL);
 
-        if ($conn->connect_error) {
-            error_log('Connection error: ' . $conn->connect_error);
-            var_dump('Connection error: ' . $conn->connect_error);
+if ($conn->connect_error) {
+    error_log('Connection error: ' . $conn->connect_error);
+    var_dump('Connection error: ' . $conn->connect_error);
+} else {
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        echo '<h3>Legacy Mobile Data</h3>';
+        echo '<table style="width: 80%">';
+        echo '<tr><th style="text-align:left">This is a Country Name</th><th style="text-align:left">Number of mobile phone providers</th></tr>';
+        while($row = $result->fetch_assoc()) {
+            echo '<tr><td>' . $row["name"] . '&nbsp</td><td>' . $row["mobilephones"] . '&nbsp</td></tr>';
         }
-        else {
-          $result = $conn->query($sql);
-          if ($result->num_rows > 0) {
+        echo '</table>';
+    }
 
-              echo '<table style="width: 80%">';
-              echo '<tr>';
-              echo '<th style="text-align:left">This is a Country Name</th>';
-              echo '<th style="text-align:left">Number of mobile phone providers</th>';
-              echo '</tr>';
-
-              while($row = $result->fetch_assoc()) {
-
-              echo '<tr>';
-              echo '<td>';
-              echo $row["name"];
-              echo '&nbsp';
-              echo '<td>';
-              echo $row["mobilephones"];
-              echo '&nbsp';
-              echo '<br>';
-              echo '</tr>';
-              }
-              echo '</table>';
-            }
-          }
+    $sql2 = "SELECT country, year, subscriptions_per_100 FROM mobile ORDER BY year ASC";
+    $result2 = $conn->query($sql2);
+    if ($result2->num_rows > 0) {
+        echo '<h3>Updated Mobile Subscriptions</h3>';
+        echo '<table style="width: 80%">';
+        echo '<tr><th>Country</th><th>Year</th><th>Subscriptions per 100</th></tr>';
+        while ($row = $result2->fetch_assoc()) {
+            echo '<tr><td>' . $row["country"] . '</td><td>' . $row["year"] . '</td><td>' . $row["subscriptions_per_100"] . '</td></tr>';
+        }
+        echo '</table>';
+    }
+}
+?>
 
 
-    ?>
+
