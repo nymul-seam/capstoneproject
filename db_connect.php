60471c0a0246f643312f07c9ad4ff7d5df52d92d
Nymul Seam <seamnymul@gmail.com>
2025-10-29 08:42:55 +1100

    Refactor database connection to use $conn variable

--- db_connect.php
+++ db_connect.php
@@ -3,22 +3,14 @@ require_once __DIR__ . '/parameters/get-parameters.php';
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-$mysqli = mysqli_init();
-mysqli_options($mysqli, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
+$conn = mysqli_init();
+mysqli_options($conn, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
 
-if (!mysqli_real_connect(
-    $mysqli,
-    $host,
-    $user,
-    $pass,
-    $db,
-    $port,
-    null,
-    MYSQLI_CLIENT_SSL
-)) {
+// TLS required on Azure MySQL. No client certs needed.
+if (!mysqli_real_connect($conn, $host, $username, $password, $db_name, $port, null, MYSQLI_CLIENT_SSL)) {
     http_response_code(500);
     exit('Database connection failed: ' . mysqli_connect_error());
 }
 
-$mysqli->set_charset('utf8mb4');
+$conn->set_charset('utf8mb4');
 ?>
