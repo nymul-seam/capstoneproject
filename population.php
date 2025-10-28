b18e170309cee1c6f609dda9e40fc69665f13906
Nymul Seam <seamnymul@gmail.com>
2025-10-28 14:38:33 +1100

    Refactor population data handling in population.php
    Refactor population data retrieval and display logic.

--- population.php
+++ population.php
@@ -9,47 +9,41 @@
   <input type="submit" id="pickquery" value="Pick another query">
 </form>
 
- <?php
-        include 'get-parameters.php';
-        //Query for the population data
-	$sql = "select name, population, populationurban from countrydata_table;";
+<?php
+include 'get-parameters.php';
 
-	$conn = mysqli_init();
-        mysqli_ssl_set($conn,NULL,NULL, "$sslcert", NULL, NULL);
-        mysqli_real_connect($conn, "$host", "$username", "$password", "$db_name", 3306, MYSQLI_CLIENT_SSL);
+$sql = "select name, population, populationurban from countrydata_table;";
+$conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL, "$sslcert", NULL, NULL);
+mysqli_real_connect($conn, "$host", "$username", "$password", "$db_name", 3306, MYSQLI_CLIENT_SSL);
 
-        if ($conn->connect_error) {
-            error_log('Connection error: ' . $conn->connect_error);
-            var_dump('Connection error: ' . $conn->connect_error);
+if ($conn->connect_error) {
+    error_log('Connection error: ' . $conn->connect_error);
+    var_dump('Connection error: ' . $conn->connect_error);
+} else {
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        echo '<h3>Legacy Population Data</h3>';
+        echo '<table style="width: 80%">';
+        echo '<tr><th>Country Name</th><th>Population</th><th>Urban Population</th></tr>';
+        while($row = $result->fetch_assoc()) {
+            echo '<tr><td>' . $row["name"] . '</td><td>' . $row["population"] . '</td><td>' . $row["populationurban"] . '</td></tr>';
         }
-        else {
-          $result = $conn->query($sql);
-          if ($result->num_rows > 0) {
-              //echo "success";
-              echo '<table style="width: 80%">';
-              echo '<tr>';
-              echo '<th style="text-align:left">Country Name</th>';
-              echo '<th style="text-align:left">Population</th>';
-              echo '<th style="text-align:left">Urban Population</th>';
-              echo '</tr>';
+        echo '</table>';
+    }
 
-              while($row = $result->fetch_assoc()) {
-              //echo '<p>';
-
-              echo '<tr>';
-              //echo 'Country_Name  Mobile_phone_subscribers  Mortality_under5';
-              echo '<td>';
-              echo $row["name"];
-              echo '&nbsp';
-              echo '<td>';
-              echo $row["population"];
-              echo '&nbsp';
-              echo '<td>';
-              echo $row["populationurban"];
-              echo '<br';
-              echo '</tr>';
-              }
-              echo '</table>';
-          }
+    $sql2 = "SELECT country, year, population FROM population ORDER BY year ASC";
+    $result2 = $conn->query($sql2);
+    if ($result2->num_rows > 0) {
+        echo '<h3>Updated Population Data</h3>';
+        echo '<table style="width: 80%">';
+        echo '<tr><th>Country</th><th>Year</th><th>Population</th></tr>';
+        while ($row = $result2->fetch_assoc()) {
+            echo '<tr><td>' . $row["country"] . '</td><td>' . $row["year"] . '</td><td>' . $row["population"] . '</td></tr>';
         }
-    ?>
+        echo '</table>';
+    }
+}
+?>
+
+
