1fdd3d00ef415fe44e9e4a0067b9d460d4dd6d72
Nymul Seam <seamnymul@gmail.com>
2025-10-29 13:54:50 +1100

    Update mortality.php

--- mortality.php
+++ mortality.php
@@ -34,29 +34,23 @@
   }
 </style>
 
-<h2>Childhood Mortality Rates</h2>
-<table border="1" cellpadding="8" cellspacing="0">
+<h2>Mortality Rate per 1000 People (2020)</h2>
+<table border="1">
   <tr>
+    <th>ID</th>
     <th>Country</th>
     <th>Year</th>
-    <th>Mortality Rate (per 1,000 live births)</th>
+    <th>Mortality Rate</th>
   </tr>
-  <tr>
-    <td>Australia</td>
-    <td>2022</td>
-    <td>3.2</td>
-  </tr>
-  <tr>
-    <td>India</td>
-    <td>2022</td>
-    <td>28.3</td>
-  </tr>
-  <tr>
-    <td>Brazil</td>
-    <td>2022</td>
-    <td>13.5</td>
-  </tr>
+  <tr><td>1</td><td>Australia</td><td>2020</td><td>6.60</td></tr>
+  <tr><td>2</td><td>Bangladesh</td><td>2020</td><td>26.90</td></tr>
+  <tr><td>3</td><td>United States</td><td>2020</td><td>8.30</td></tr>
+  <tr><td>4</td><td>India</td><td>2020</td><td>6.00</td></tr>
+  <tr><td>5</td><td>Brazil</td><td>2020</td><td>13.20</td></tr>
 </table>
+
+
+
 
 
 
