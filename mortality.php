1ffce0c8131a76f643932d28ff245f4ff7bdd8b8
Nymul Seam <seamnymul@gmail.com>
2025-10-29 12:28:03 +1100

    Refactor mortality.php to include static data
    Removed legacy mortality query and added static childhood mortality rates.

--- mortality.php
+++ mortality.php
@@ -1,50 +1,27 @@
-<style>
-#pickquery {
-     height: 50px;
-     width: 150px;
-}
-</style>
+<h2>Childhood Mortality Rates</h2>
+<table border="1" cellpadding="8" cellspacing="0">
+  <tr>
+    <th>Country</th>
+    <th>Year</th>
+    <th>Mortality Rate (per 1,000 live births)</th>
+  </tr>
+  <tr>
+    <td>Australia</td>
+    <td>2022</td>
+    <td>3.2</td>
+  </tr>
+  <tr>
+    <td>India</td>
+    <td>2022</td>
+    <td>28.3</td>
+  </tr>
+  <tr>
+    <td>Brazil</td>
+    <td>2022</td>
+    <td>13.5</td>
+  </tr>
+</table>
 
-<form action='query.php' size=20px>          
-  <input type="submit" id="pickquery" value="Pick another query">
-</form>
 
-<?php
-include 'get-parameters.php';
 
-// Legacy query for childhood mortality
-$sql = "SELECT name, mortalityunder5 FROM countrydata_table;";
-$conn = mysqli_init();
-mysqli_ssl_set($conn, NULL, NULL, "$sslcert", NULL, NULL);
-mysqli_real_connect($conn, "$host", "$username", "$password", "$db_name", 3306, MYSQLI_CLIENT_SSL);
-
-if ($conn->connect_error) {
-    error_log('Connection error: ' . $conn->connect_error);
-    var_dump('Connection error: ' . $conn->connect_error);
-} else {
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-        echo '<h3>Legacy Childhood Mortality Data</h3>';
-        echo '<table style="width: 80%">';
-        echo '<tr><th style="text-align:left">Country Name</th><th style="text-align:left">Childhood Mortality</th></tr>';
-        while ($row = $result->fetch_assoc()) {
-            echo '<tr><td>' . $row["name"] . '</td><td>' . $row["mortalityunder5"] . '</td></tr>';
-        }
-        echo '</table>';
-    }
-
-    // New query from mortality table
-    $sql2 = "SELECT country, year, mortality_rate_per_1000 FROM mortality ORDER BY year ASC";
-    $result2 = $conn->query($sql2);
-    if ($result2->num_rows > 0) {
-        echo '<h3>Updated Mortality Rate Data</h3>';
-        echo '<table style="width: 80%">';
-        echo '<tr><th style="text-align:left">Country</th><th style="text-align:left">Year</th><th style="text-align:left">Mortality Rate per 1,000</th></tr>';
-        while ($row = $result2->fetch_assoc()) {
-            echo '<tr><td>' . $row["country"] . '</td><td>' . $row["year"] . '</td><td>' . $row["mortality_rate_per_1000"] . '</td></tr>';
-        }
-        echo '</table>';
-    }
-}
-?>
 
