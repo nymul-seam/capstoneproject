5efa63449286b65bf69edc5ca9ac2a362a0a138b
Nymul Seam <seamnymul@gmail.com>
2025-10-29 13:50:05 +1100

    Update lifeexpectancy.php

--- lifeexpectancy.php
+++ lifeexpectancy.php
@@ -35,29 +35,23 @@
 </style>
 
 
-<h2>Life Expectancy by Country</h2>
-<table border="1" cellpadding="8" cellspacing="0">
+<h2>Life Expectancy (2020)</h2>
+<table border="1">
   <tr>
+    <th>ID</th>
     <th>Country</th>
     <th>Year</th>
-    <th>Life Expectancy (years)</th>
+    <th>Life Expectancy</th>
   </tr>
-  <tr>
-    <td>Australia</td>
-    <td>2022</td>
-    <td>83.2</td>
-  </tr>
-  <tr>
-    <td>India</td>
-    <td>2022</td>
-    <td>70.8</td>
-  </tr>
-  <tr>
-    <td>Brazil</td>
-    <td>2022</td>
-    <td>75.9</td>
-  </tr>
+  <tr><td>1</td><td>Australia</td><td>2020</td><td>83.4</td></tr>
+  <tr><td>2</td><td>Bangladesh</td><td>2020</td><td>72.6</td></tr>
+  <tr><td>3</td><td>United States</td><td>2020</td><td>78.8</td></tr>
+  <tr><td>4</td><td>India</td><td>2020</td><td>69.7</td></tr>
+  <tr><td>5</td><td>Brazil</td><td>2020</td><td>75.9</td></tr>
 </table>
+
+
+
 
 
 
